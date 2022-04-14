58ad7c4272caca438e9ab69df93f120923c4cb9d
Aleksey Valouev <alekseyvalouev@gmail.com>
2022-04-14 14:37:32 -0700

    finished basic strategy

--- TestStrat.py
+++ TestStrat.py
@@ -53,22 +53,78 @@ class StockFetcher:
         for period in periods:
             self.plot_variable('MA %s' % str(period))
 
-    def MA_Signals(self, p1, p2):
+    def MA_Signals(self, p1, p2, p3):
         self.signals = pd.DataFrame(index=self.df.index)
+        # copy moving averages from main dataframe
         self.signals['MA %i' % p1] = self.df['MA %i' % p1]
         self.signals['MA %i' % p2] = self.df['MA %i' % p2]
-        self.signals['signal'] = 0.0
-        self.signals['signal'][p1:] = np.where(self.signals['MA %i' % p1][p1:] > self.signals['MA %i' % p2][p1:], 1.0, 0.0)  
-        self.signals['positions'] = self.signals['signal'].diff()
+        self.signals['MA %i' % p3] = self.df['MA %i' % p3]
+        # define temporary signal columns
+        self.signals['signal_point_one'] = 0.0
+        self.signals['signal_point_two'] = 0.0
+        # target price (exit)
+        self.signals['target'] = np.NaN
+        # first point in fibonacci sequence
+        self.signals['p1_numerical'] = np.NaN
+        # second point in fibonacci sequence
+        self.signals['p2_numerical'] = np.NaN
+        # find signals
+        self.signals['signal_point_one'][p1:] = np.where(self.signals['MA %i' % p2][p1:] > self.signals['MA %i' % p1][p1:], 1.0, 0.0)  
+        self.signals['signal_point_two'][p1:] = np.where(self.signals['MA %i' % p3][p1:] > self.signals['MA %i' % p2][p1:], 1.0, 0.0)  
+        # clean signal columns
+        self.signals['first_points'] = self.signals['signal_point_one'].diff()
+        self.signals['second_points'] = self.signals['signal_point_two'].diff()
+        # 30 day rolling average
+        self.signals['30_rolling'] = self.df['Adj Close'].rolling(30).mean()
+        # create first two points
+        self.signals['p1_numerical'][:] = np.where(self.signals['first_points'][:] > 0, self.signals['30_rolling'], 0.0)
+        self.signals['p2_numerical'][:] = np.where(self.signals['second_points'][:] > 0, self.signals['30_rolling'], 0.0)
+        # create temporary running first point array
+        self.signals['filled_p1'] = self.signals['p1_numerical'].replace(to_replace=0, method='ffill')
+        # create target prices
+        self.signals['target'] = np.where(self.signals['p2_numerical'] > 0, self.signals['filled_p1'] + 0.618*(self.signals['filled_p1']-self.signals['p2_numerical']), 0)
+        self.signals['target'] = self.signals['target'].replace(to_replace=0, method='ffill')
+        # now create dummy variable to track when close price is > target price
+        self.signals['positions_sell'] = np.where(self.signals['target'] < self.df['Adj Close'], -1.0, 0.0)
+        self.signals['positions_sell'] = self.signals['positions_sell'].diff()
+        self.signals['positions_buy'] = np.where(self.signals['second_points'] > 0, 1.0, 0.0)
+        self.signals['positions_buy'] = self.signals['positions_buy'].diff()
+        # fill final positions column with positions from both buy and sell columns
+        self.signals['signal'] = self.signals['positions_sell']
+        self.signals['signal'] = np.where(self.signals['positions_buy'] > 0, 1.0, self.signals['signal'])
 
-    def plot_MA_signals(self, p1, p2):
-        plt.plot(self.signals.loc[self.signals.positions == 1.0].index, 
-                self.signals['MA %i' % p1][self.signals.positions == 1.0],
-                '^', markersize=10, color='m')
-                
-        plt.plot(self.signals.loc[self.signals.positions == -1.0].index, 
-                self.signals['MA %i' % p2][self.signals.positions == -1.0],
-                'v', markersize=10, color='k')
+    def plot_fibonacci_extensions(self):
+        plt.plot(self.signals.loc[self.signals['positions_buy'] > 0].index, 
+                self.df['Adj Close'][self.signals['positions_buy'] > 0],
+                '^', markersize=10, color='g')
+
+        plt.plot(self.signals.loc[self.signals['positions_sell'] < 0].index, 
+                self.df['Adj Close'][self.signals['positions_sell'] < 0],
+                'v', markersize=10, color='r')
+
+        plt.plot(self.signals['target'], '--')
+
+    def back_test(self):
+        initial_capital= float(100000.0)
+
+        positions = pd.DataFrame(index=self.signals.index).fillna(0.0)
+
+        positions['AAPL'] = 100*self.signals['signal'].cumsum()   
+        
+        self.portfolio = positions.multiply(self.df['Adj Close'], axis=0)
+
+        pos_diff = positions.diff()
+
+        self.portfolio['holdings'] = (positions.multiply(self.df['Adj Close'], axis=0)).sum(axis=1)
+
+        self.portfolio['cash'] = initial_capital - (pos_diff.multiply(self.df['Adj Close'], axis=0)).sum(axis=1).cumsum()   
+
+        self.portfolio['total'] = self.portfolio['cash'] + self.portfolio['holdings']
+
+        self.portfolio['returns'] = self.portfolio['total'].pct_change()
+
+    def plot_total(self):
+        self.portfolio['total'].plot(lw=2.)
 
     def get_df(self):
         return self.df
@@ -82,14 +138,20 @@ class MA_Strategy:
             df['MA %s' % str(period)] = df['Adj Close'].rolling(period).mean()
         return df
 
+class Fibonacci_Calculator:
+    def __init__(self):
+        pass
+
 if (__name__ == "__main__"):
-    myStockFetcher = StockFetcher('AAPL', '2020-9-1', '2022-1-1')
+    myStockFetcher = StockFetcher('USO', '2019-1-1', '2022-1-1')
     myStockFetcher.calculate_percent_change()
-    myStockFetcher.plot_variable('Adj Close')
     myStockFetcher.apply_MA([10, 40, 100])
-    myStockFetcher.plot_MA([10, 40, 100])
-    myStockFetcher.MA_Signals(10, 40)
-    myStockFetcher.plot_MA_signals(10, 40)
+    myStockFetcher.MA_Signals(10, 40, 100)
+    #myStockFetcher.plot_MA([10, 40, 100])
+    #myStockFetcher.plot_variable('Adj Close')
+    #myStockFetcher.plot_fibonacci_extensions()
+    myStockFetcher.back_test()
+    myStockFetcher.plot_total()
     plt.show()
     
         
